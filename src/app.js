01c84d6cde5e09e78a584a10097f208a70fdcdab
Aubrey Barnes <aubria6@hotmail.com>
2021-02-26 11:13:35 -0600

    update fetch to return an array of data

--- src/app.js
+++ src/app.js
@@ -1,4 +1,21 @@
 fetch(`/.netlify/functions/fetch-recent`)
     .then(res => {
-        console.log(res.json())
+        // console.log(res.json())
+        return res.json()
+        
     })
+    .then(data => {
+        console.log(data.drinks)
+    })
+
+    // function fetchDemo() {
+    //     return fetch(countriesUrl).then(function(response) {
+    //         return response.json();
+    //     }).then(function(json) {
+    //         return json;
+    //     });
+    // }
+
+    // fetchDemo().then(function(result) {
+    //     console.log(result);
+    // });
